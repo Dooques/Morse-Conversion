2ca9507665d44735059dc232727e6deed343dd0c
Sam Dukes <samuelgdukes@gmail.com>
2024-06-30 15:34:56 +0100

    Complete functionality of Flask, HTML & Class

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import csv
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from wtforms import form
+from morse_converter import Morse
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 
+morse = Morse()
 
-@app.route('/')
+
+@app.route('/', methods=['get', 'post'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        to_convert = request.form['to_convert']
+        converted_string = morse.convert_string(to_convert)
+        print(converted_string)
+        return render_template('home.html', converted_string=converted_string, to_convert=to_convert)
+    return render_template('home.html', to_convert=" ")
 
 
 if __name__ == "__main__":
--- morse_converter.py
+++ morse_converter.py
@@ -6,22 +6,27 @@ class Morse:
     def __init__(self):
         self.alphabet = []
         self.morse_code = []
-
-    def convert_string(self, to_convert):
-        print('Welcome to the Morse Converter')
-        to_convert = input("Please enter a word or phrase you wish to convert: ")
         with open('files/morse.csv', newline='') as csv_file:
             morse = csv.reader(csv_file, delimiter=' ', quotechar='|')
             for row in morse:
                 entry = ', '.join(row).split(',')
                 self.alphabet.append(entry[0])
                 self.morse_code.append(entry[1])
+        self.morse_alphabet = pd.DataFrame({'alphabet': self.alphabet, 'morse_code': self.morse_code})
 
-        morse_alphabet = pd.DataFrame({'alphabet': self.alphabet, 'morse_code': self.morse_code})
-
+    def convert_string(self, to_convert):
+        print("String received")
         morse_string = []
         for char in to_convert:
-            char_entry = morse_alphabet.loc[morse_alphabet.alphabet == char.capitalize()]
-            morse_string.append(char.capitalize() + ': ' + char_entry.morse_code.values[0])
-
-        print(f'Your converted phrase is: {", ".join(morse_string)}')
+            char_cap = char.capitalize()
+            print(char_cap)
+            if char_cap in self.morse_alphabet.alphabet.values:
+                print(f' Searching DF {char}')
+                print("char found")
+                char_entry = self.morse_alphabet.loc[self.morse_alphabet.alphabet == char_cap]
+                print(char_entry.morse_code.values)
+                morse_string.append(char_entry.morse_code.values[0])
+        print(morse_string)
+        result = " ".join(morse_string)
+        print(result)
+        return result
